e578aca0f1c9a996e58fd1a1d9b75c3be787b254
Vault1996 <vault1996@gmail.com>
2016-07-14 22:04:57 +0300

    Delete

--- 05_Parsing_Ivashkyavichyus_Algis/src/by/epam/parsing/action/interpreter/AbstractMathExpression.java
+++
@@ -1,7 +0,0 @@
-package by.epam.parsing.action.interpreter;
-
-import by.epam.parsing.exception.CantInterpretException;
-
-public abstract class AbstractMathExpression {
-	public abstract void interpret(Context context) throws CantInterpretException;
-}
