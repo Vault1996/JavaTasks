3af8d1019c5358ff828c6c7ec1ce96d3f018be30
Vault1996 <vault1996@gmail.com>
2016-09-29 05:10:47 +0300

    Final

--- cinemarating/src/tests/Tests.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-import java.sql.Timestamp;
-
-public class Tests {
-	public static void main(String[] args) {
-		System.out.println(new Timestamp(System.currentTimeMillis()));
-	}
-}
